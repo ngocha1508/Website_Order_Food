2df9f108fdec2fedc9d921495ca609d9314db1eb
ngocha1508 <124545235+ngocha1508@users.noreply.github.com>
2024-10-04 11:11:17 +0700

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://haduongngoc150803:Ha123456789@cluster0.nyr8qnq.mongodb.net/web_order_food').then(()=>console.log("DB Connected"));
-}
